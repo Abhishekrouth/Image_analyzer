08720056eb349364272da12586a2aef4fda25b02
Abhishek Routh <routhabhishek6@gmail.com>
2025-10-31 16:06:10 +0530

    Added feature to translate the extracted text

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This is a Flask based Image analyzer built using Google Cloud Vision API. It analyzes
 uploaded image and returns Labels, Likelihood, Text, and properties of the image.
+If the there's text on the image then it will be translated into the targeted language. 
 
 ## Features:
 
@@ -9,24 +10,40 @@ uploaded image and returns Labels, Likelihood, Text, and properties of the image
 * body:
        requests: to read image
        features: 
+
                 type (LABEL_DETECTION):To return the details of the image like description, score etc.
+
                 type (TEXT_DETECTION): To return the text content on the image.
+
                 type (IMAGE_PROPERTIES): To return the dominantColors, pixelFraction etc.
+
                 type (FACE_DETECTION): To retun the likelyhood of the image, example: joyLikelihood etc.
-        
-        maxResults: To set the maximum number of output.
+
+                maxResults: To set the maximum number of output.
+
+* body_translation: 
+              
+                "q" : for the text to be translated
+
+                "target": language in which extracted text will be translated
+
+                "format": format of the source text
 
 ## Technologies used:
 
 1. Flask
-2. Google cloud vision 
+2. Google cloud vision API
+3. Translation API
 
+### Translation
+
+![alt text](images/Translation(image11).png)
 
 ### Face detection
         
 ![alt text](images/Face_detection(image9).png)
 
-### Label Detection
+### Label Detection and Image properties
 
 ![alt text](images/Label_detection(image3).png)
 
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import base64
 import os
 from dotenv import load_dotenv
+import re
 
 app = Flask(__name__)
 
@@ -17,6 +18,14 @@ def bad_request(e):
         "message": "Bad Request. Image file is missing."
     }), 400
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({
+        "status": "error",
+        "code": 404,
+        "message": "Target language is missing."
+    }), 404
+
 @app.route("/image_analyzer", methods=["POST"])
 def image_analyzer():
     if "image_file" not in request.files:
@@ -27,6 +36,7 @@ def image_analyzer():
     img= image_content.decode("utf-8")
 
     url = f"https://vision.googleapis.com/v1/images:annotate?key={google_api_key}"
+    url_for_translation = f"https://translation.googleapis.com/language/translate/v2?key={google_api_key}"
 
     body = {
         "requests": [
@@ -58,6 +68,7 @@ def image_analyzer():
     }
     response = requests.post(url, json=body)
     result = response.json()
+    text = result["responses"][0].get("textAnnotations",[])
     face = result["responses"][0].get("faceAnnotations",[])
     if face:
         exp1 = face[0]["joyLikelihood"]
@@ -69,20 +80,40 @@ def image_analyzer():
                         "sorrowLikelihood": exp2,
                         "angerLikelihood": exp3,
                         "supriseLikelihood": exp4
-                        }})
+                        },
+                        "message": "No text or labels detected in the image"
+                        })
+    elif text:
+        text = result["responses"][0].get("textAnnotations",[])
+        des = text[0]["description"]
+        final_des = des.replace('\n', ' ').replace('\r', '')
+        target_language = request.form.get("target_language")
+        if not target_language:
+            abort(404)
+
+        body_translation = {
+            "q": final_des,
+            "target": target_language,
+            "format": "text"
+    }
+        response_translation = requests.post(url_for_translation, json=body_translation)
+        result_translation = response_translation.json()
+        translated = result_translation["data"]["translations"][0]["translatedText"]
+        final_translated = translated.replace('\n', '').replace('\r', '')
+        return jsonify({
+            "Original_text": final_des,
+            "target language": target_language,
+            "Translated_text": final_translated,
+            "message": "No face or label detected in the image"            
+        })
     else:
         labels = result["responses"][0].get("labelAnnotations",[])
-        text = result["responses"][0].get("textAnnotations",[])
         img_prop= result["responses"][0].get("imagePropertiesAnnotation",[])
-        if text:
-            des = text[0]["description"]
-        else:
-            des = "No text detected"
         
         return jsonify({
             "Labels": labels,
-            "text": des,
-            "image_properties": img_prop
+            "image_properties": img_prop,
+            "message": "No text or faces detected in the image"
     })
 
 if __name__ == "__main__":
